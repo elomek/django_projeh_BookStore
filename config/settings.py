c204c145f31c14883b411b2633938e56c27a3916
Elham Karamian <elhamkaramian@MacBook-Pro-de-Elham.local>
2023-04-03 15:39:22 +0200

    add static directory

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -139,3 +139,6 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS_DIRS = [str(BASE_DIR.joinpath('static'))]
