f0e86e64c973ede7c696db82e4ce68a2f421bbbc
Elham Karamian <elhamkaramian@MacBook-Pro-de-Elham.local>
2023-04-18 10:51:24 +0200

    create CommentAdmin and BookAdmin in admin.py

--- books/admin.py
+++ books/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from .models import Book, Comment
 
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+
+
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'text', 'datetime_created',)
 
 
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Comment, CommentAdmin)
 
 
