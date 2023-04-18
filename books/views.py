52ceb818cb473c0b1a509ba9cf1df5cde25b1748
Elham Karamian <elhamkaramian@MacBook-Pro-de-Elham.local>
2023-04-18 15:07:46 +0200

    add book comment to detail view

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from .models import Book
+from django.shortcuts import get_object_or_404, render
+from .models import Book, Comment
 
 
 class BookListView(generic.ListView):
@@ -10,14 +11,21 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
+# class BookDetailView(generic.DetailView):
+    # model = Book
+    # template_name = 'books/book_detail.html'
+
+def book_detail_view(request, pk):
+    # get book
+    book = get_object_or_404(Book, pk=pk)
+    # get book_comment
+    book_comment = book.comments.all()
+    return render(request, 'books/book_detail.html', {'book': book, 'comments': book_comment})
 
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price','cover',]
+    fields = ['title', 'author', 'description', 'price', 'cover', ]
     template_name = 'books/book_create.html'
 
 
@@ -31,4 +39,7 @@ class BookDeleteView(generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
+
+
+
 
